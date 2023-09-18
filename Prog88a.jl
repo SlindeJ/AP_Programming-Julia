892d032a16405489c5ecadc59d283cd230e87dd4
SlindeJ <112727654+SlindeJ@users.noreply.github.com>
2023-09-18 14:38:46 +0000

    update

--- Prog88a.jl
+++ Prog88a.jl
@@ -4,13 +4,28 @@ num1 = parse(Int64, num1)
 print("Please enter a number between 2-20: ")
 num2 = readline()
 num2 = parse(Int64, num2)
-print(abs(-2))
 
-if (1 <= num1 <= 13) #and (2 <= num2 <= 20)
-  println("Original number are $num1 and $num2")
-  sum = num1 + num2
-  println("Sum = $sum")
-  
+
+if (1 <= num1 <= 13)
+  if (2 <= num2 <= 20)
+    println("Original number are $num1 and $num2")
+    sum = num1 + num2
+    println("Sum = $sum")
+    diff = num2 - num1
+    println("Difference = $diff")
+    product = num1 * num2
+    println("Product = $product")
+    avg = sum / 2
+    println("Average = $avg")
+    absval = abs(diff)
+    println("Absolute Value = $absval")
+    max = (absval + sum) / 2
+    min = sum - max
+    println("Maximum = $max")
+    println("Minimum = $min")
+  else
+    println("Please enter valid numbers")
+  end
 
 else 
   println("Please enter valid numbers")
---
+++ StrInt12.jl
@@ -0,0 +1,10 @@
+print("Please enter a sentence to reverse: ")
+str = readline()
+# need to take in a string and reverse the order of the words in it: like "dont want to" to "to want dont" 
+#make the revstr string
+for s in str  # for space in string
+  starray = [s]
+end
+a = [1 2 3 4]
+println("$a")
+
